21ef6f2c81a4505ca401aef6cbee79729c28b3ff
Anathos Noredhel <90454112+AnathosN@users.noreply.github.com>
2023-02-02 21:02:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import json
+import os
 
 app = Flask(__name__)
 
@@ -8,14 +9,17 @@ def register():
     if request.method == "POST":
         identifier = request.form["identifier"]
         hex_key = request.form["hex_key"]
-        with open(".well-known/nostr.json", "r") as f:
-            data = json.load(f)
+        filename = ".well-known/nostr.json"
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                data = json.load(f)
+        else:
+            data = {"names": {}}
         data["names"][identifier] = hex_key
-        with open(".well-known/nostr.json", "w") as f:
+        with open(filename, "w") as f:
             json.dump(data, f, indent=4)
         return "Identifier and hex key registered successfully!"
     return render_template("register.html")
 
 if __name__ == "__main__":
     app.run()
-
