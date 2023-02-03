e9ecd473723bbcd520e8599e471a551283fabbbd
Anathos Noredhel <90454112+AnathosN@users.noreply.github.com>
2023-02-03 19:05:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def display_all():
             data = json.load(f)
             content = ""
             for identifier, hex_key in data["names"].items():
-                content += data["names"]
+                content = data["names"]
             return render_template("list.html", content=content)
     else:
         return "nostr.json file not found!"
