797edb342993ac90175a7546fa99bbd4839cbe18
Anathos Noredhel <90454112+AnathosN@users.noreply.github.com>
2023-02-03 14:54:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 import json
 import os
 
