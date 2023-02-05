fdc78c14577df062ee9a629a98034747fd87f73a
Anathos Noredhel <90454112+AnathosN@users.noreply.github.com>
2023-02-05 10:59:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -137,9 +137,6 @@ def delete():
             session["hex_key"] = hex_key
             # Render the confirm deletion page
             return redirect(url_for("confirm_delete")
-        else:
-            # Identifier not found
-            return "This identifier does not exist."
     return render_template("delete.html")
 
 @app.route("/confirm_delete", methods=["GET", "POST"])
